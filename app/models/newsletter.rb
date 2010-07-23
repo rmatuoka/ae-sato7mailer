4d5a92aaa6e9ec3b14ca0f6a3e575cc2b7807086
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2010-07-23 17:48:41 -0300

    Ajustes

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,9 +1,9 @@
 class Newsletter < ActionMailer::Base
   
     #se precisar os parametros são Email.deliver_padrao(corpo, "Assunto",email para&nbsp;&nbsp; #enviar(destino), email para resposta)
-    def enviar(corpo, assunto, email = "noreply@fasterm.com.br",responder = "noreply@fasterm.com.br")
+    def enviar(corpo, assunto, email = "juntoscom@betotricoli.com.br",responder = "juntoscom@betotricoli.com.br")
         recipients email
-        from "Nautilus <noreply@fasterm.com.br>"
+        from "Beto Tricoli <juntoscom@betotricoli.com.br>"
         subject assunto
         reply_to responder
         body :corpo => corpo
